46b5dd89c3163cd95b8dc16b1501f26bc8cfc4df
Sandi <sandi.uniska@gmail.com>
2020-04-26 19:27:32 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Sandi";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.89;
+
+
+}
